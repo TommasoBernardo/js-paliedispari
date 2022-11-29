3f0eaf4606e4295c31f2f3ac7de98912bddaad63
TommasoBernardo <tommasob1210@gmail.com>
2022-11-29 18:18:56 +0100

    finish palindroma

--- palindroma/script.js
+++ palindroma/script.js
@@ -4,3 +4,27 @@ Palidroma
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
+function Palindrome(word){
+
+    let characters=[];
+
+    for(let i=word.length; i>=0; i--){
+        characters.push(word[i]);
+    }
+
+    for(let i=0;i<parola.length;i++){
+
+    if(parola[i]!=characters[i+1]){
+        console.log('la parola non è palindroma');
+        return false;
+    }
+    }
+    console.log('la parola è palindroma');
+    return true;
+}
+
+const parola=prompt("Inserisci una parola");
+
+let palindroma=Palindrome(parola);
+
+console.log(palindroma);
